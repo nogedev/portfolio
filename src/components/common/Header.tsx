4e6f8819444c978c56a9474fdcdf8d75c382ceb7
Georges Nodari <ngeorges.dev@gmail.com>
2025-05-22 23:22:44 +0200

    feat: implement Header component with responsive navigation and fade-in animation

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,122 @@
+import { useEffect, useState } from "preact/hooks";
+
+const menuItems = [
+  { name: "Home", href: "/" },
+  { name: "Works", href: "/works" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+];
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+  return (
+    <>
+      <header className="relative z-50 w-full border-b border-gray-100 bg-white px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto flex max-w-7xl items-center justify-between py-8">
+          <a href="/" className="flex shrink-0 items-center">
+            <img
+              src="/noge-logo.png"
+              alt="noge logo"
+              className="h-8 w-auto object-contain"
+            />
+          </a>
+
+          <div className="hidden items-center gap-6 md:flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 text-black"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+              />
+            </svg>
+
+            <nav className="flex items-center gap-6 mt-0.5">
+              {menuItems.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-sm font-medium  text-black hover:text-black/40 transition-colors duration-300 ease-in-out"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </nav>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 text-black"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+              />
+            </svg>
+          </div>
+
+          <button
+            className="md:hidden"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-10 text-black"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 9h16.5m-16.5 6.75h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </header>
+
+      {isOpen && (
+        <div
+          className="fixed inset-0 top-[88px] z-40 flex flex-col items-center justify-center space-y-6 bg-white px-4 py-10 md:hidden
+    animate-fade-in"
+        >
+          {menuItems.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-7xl font-bold text-black uppercase hover:text-black/40 transition-colors duration-300 ease-in-out"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Header;
