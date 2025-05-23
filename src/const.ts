93fcdf31a350d4e2a0d1d41d7517932b313da2ca
Georges Nodari <ngeorges.dev@gmail.com>
2025-05-23 19:30:10 +0200

    feat: Refactor ConnectLinks component to use centralized connectLinksItems and add Netlify configuration

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,14 @@
 export const SITE_TITLE = "NOGE";
 export const SITE_DESCRIPTION =
   "Welcome to my portfolio where I showcase my work, services, and more.";
+
+export const connectLinksItems = [
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/georges-nodari/",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/noge-dev",
+  },
+];
