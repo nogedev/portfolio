cda7f7dfb0bb6e98ab0da3dbfee87d6cafefda72
Georges Nodari <ngeorges.dev@gmail.com>
2025-06-23 02:45:47 +0200

    refactor: Remove unused SVG icons from menu toggle button

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -115,36 +115,6 @@ const Header = () => {
                 </svg>
               )
             }
-
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="size-10 text-black"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 18 18 6M6 6l12 12"
-              />
-            </svg> */}
-
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-10 text-black"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 9h16.5m-16.5 6.75h16.5"
-              />
-            </svg> */}
           </button>
         </div>
       </header>
