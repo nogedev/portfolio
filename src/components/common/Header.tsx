224081dfd23707d708f38aa2e0581d83f8fe3d26
Georges Nodari <ngeorges.dev@gmail.com>
2025-08-06 21:55:13 +0200

    chore: add @netlify/blobs dependency to package.json and package-lock.json

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -34,47 +34,18 @@ const Header = () => {
           </a>
 
           <div className="hidden items-center gap-6 md:flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-6 text-black"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
-              />
-            </svg>
 
             <nav className="flex items-center gap-6 mt-0.5">
               {menuItems.map((item) => (
                 <a
                   key={item.name}
                   href={item.href}
-                  className="text-sm font-semibold text-black hover:text-black/40 transition-colors duration-300 ease-in-out"
+                  className="text-lg font-semibold text-black hover:text-black/40 transition-colors duration-300 ease-in-out"
                 >
                   {item.name}
                 </a>
               ))}
             </nav>
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-6 text-black"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
-              />
-            </svg>
           </div>
 
           <button
